76a4a2870f54c1594845711ae26bf2709ce6e39a
Oleg Gorbunov <beornborn@gmail.com>
2016-10-10 14:03:15 +0300

    poh

--- lib/klass.rb
+++ lib/klass.rb
@@ -2,8 +2,50 @@ require 'awesome_print'
 require 'byebug'
 require 'active_support/all'
 
+# Vending machine has following coins: 1c, 5c, 10c, 25c, 50c, 1$.
+# getChange(5, 0.99) should return [1,0,0,0,0,4]
 class Klass
-  def self.meth arg
-    [1,2,3].blank?
+  CAP_SYM = ['!', '?', '.'].freeze
+  NON_CAP_SYM = [':', ';'].freeze
+
+  def self.handle_string(input)
+    return '' if input.blank?
+
+    input_arr = input.split('')
+    result_arr = [input_arr.shift.capitalize]
+
+    current_index = 0
+    do_next_space = false
+    do_next_cap = false
+    while !input_arr[current_index].nil?
+      el = input_arr[current_index]
+
+      if do_next_space && el != ' '
+        result_arr << el
+        do_next_space = false
+      end
+
+      if do_next_cap && el != ' ' && !el.in(CAP_SYM) && !el.in(NON_CAP_SYM)
+        result_arr << el.capitalize
+        do_next_cap = false
+        next
+      end
+    end
+
+    result.join
   end
 end
+
+
+
+# Write a program that will correct the input string to use proper capitalization and spacing.
+
+# Allowed punctuations are the period ( . ), question mark ( ? ), and exclamation ( ! ) (...).
+
+# Make sure that colons ( : ) and semicolons ( ; ) and all the punctuations are always followed by spaces. Input string will be a valid English sentence.
+
+# Example: "first, solve the problem.then, write the code."
+# Output:  "First, solve the problem. Then, write the code."
+
+# Example: "this is a test... and another test."
+# Output:  "This is a test... And another test."
--- spec/klass_spec.rb
+++ spec/klass_spec.rb
@@ -2,6 +2,26 @@ require_relative 'spec_helper'
 
 describe Klass do
   it "should return result" do
-    expect(described_class.meth(1)).to eq false
+    str = 'first, solve the problem.then, write the code.'
+    solution = "First, solve the problem. Then, write the code."
+    expect(described_class.handle_string(str)).to eq solution
+    # str = "this is a test... and another test."
+    # solution = "This is a test... And another test."
+    # expect(described_class.handle_string(str)).to eq solution
   end
 end
+
+# getChange(3.14, 1.99) should return [0,1,1,0,0,1]
+# getChange(4, 3.14) should return [1,0,1,1,1,0]
+# getChange(0.45, 0.34) should return [1,0,1,0,0,0]
+# Write a program that will correct the input string to use proper capitalization and spacing.
+
+# Allowed punctuations are the period ( . ), question mark ( ? ), and exclamation ( ! ) (...).
+
+# Make sure that colons ( : ) and semicolons ( ; ) and all the punctuations are always followed by spaces. Input string will be a valid English sentence.
+
+# Example: "first, solve the problem.then, write the code."
+# Output:  "First, solve the problem. Then, write the code."
+
+# Example: "this is a test... and another test."
+# Output:  "This is a test... And another test."
